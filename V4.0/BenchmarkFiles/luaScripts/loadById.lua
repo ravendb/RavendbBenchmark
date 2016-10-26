9c12eb15e8eb8e6adb63855573388fae48f67dd4
Ubuntu <ubuntu@ip-172-31-35-26.us-west-2.compute.internal>
2016-10-26 18:46:52 +0000

    Adding additional scripts

--- V4.0/BenchmarkFiles/luaScripts/loadById.lua
+++ V4.0/BenchmarkFiles/luaScripts/loadById.lua
@@ -1,4 +1,8 @@
 request = function()
-   path = "/databases/BenchmarkDB/docs?&id=disks%2F".. math.random(3500000)..""
-   return wrk.format(nil, path)
+	if math.random(2) == 1 then 
+		path = "/databases/StackOverflow/docs?&id=questions/".. math.random(12350817)..""
+	else
+		  path = "/databases/StackOverflow/docs?&id=users/".. math.random(5987285)..""
+	end
+   	return wrk.format(nil, path)
 end
